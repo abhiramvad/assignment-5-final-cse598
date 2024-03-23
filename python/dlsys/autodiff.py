49c4eb4e3ab511255d5bc278fb2894b53d162aa1
Abhiram Vadlapatla <v.abhiram97@gmail.com>
2024-03-23 12:13:43 -0700

    updated autodiff

--- python/dlsys/autodiff.py
+++ python/dlsys/autodiff.py
@@ -153,8 +153,7 @@ class AddOp(Op):
     def infer_shape(self, node, input_shapes):
         """Need to handle input_vals[0].shape != input_vals[1].shape"""
         """TODO: Your code here"""
-        assert input_shapes[0] == input_shapes[1]
-        return input_shapes[0]
+        return broadcast_rule(input_shapes[0], input_shapes[1])
 
 
 class AddByConstOp(Op):
